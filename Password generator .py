b0c2593188cc1a42bd9a6ad28a41ded387f4447e
Jannyboy <janorzegwydra29@gmail.com>
2020-04-05 19:49:49 +0100

    no message

---
+++ Password generator .py
@@ -0,0 +1,20 @@
+import random
+
+print('''
+Password Generator
+==================
+''')
+
+Chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?£'
+
+number = input('Number of passwords? - ')
+number = int(number)
+
+length = input('Password length? - ')
+length = int(length)
+
+for p in range(number):
+    password = '' 
+    for c in range(length):
+        password += random.choice(Chars)
+    print(password)
